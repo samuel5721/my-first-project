0efda9f59fdefd4563ee4096cdfb6d27c3f7189b
Kim Samuel <samuel20070731@gmail.com>
2023-07-04 22:09:43 +0900

    계산기 Ui 만듬

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import "./calculatorTable.css"; 
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const addCounter = () => setCounter((counter) => counter + 1);
-  console.log("api 호출");
-  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>{counter}</h2>
-        <button onClick={addCounter}>click me</button>
-      </header>
+      <h1>계산기</h1>
+      <div id="calculator">
+        <div id="window">0</div>
+        <div className='calRow'>
+          <button className='calBtn'>◀</button>
+          <button className='calBtn'>C</button>
+          <button className='calBtn'>%</button>
+          <button className='calBtn'>÷</button>
+        </div>
+        <div className='calRow'>
+          <button className='calBtn'>7</button>
+          <button className='calBtn'>8</button>
+          <button className='calBtn'>9</button>
+          <button className='calBtn'>×</button>
+        </div>
+        <div className='calRow'>
+          <button className='calBtn'>4</button>
+          <button className='calBtn'>5</button>
+          <button className='calBtn'>6</button>
+          <button className='calBtn'>-</button>
+        </div>
+        <div className='calRow'>
+          <button className='calBtn'>1</button>
+          <button className='calBtn'>2</button>
+          <button className='calBtn'>3</button>
+          <button className='calBtn'>+</button>
+        </div>
+        <div className='calRow'>
+          <button className='calBtn'>+/-</button>
+          <button className='calBtn'>0</button>
+          <button className='calBtn'>.</button>
+          <button className='calBtn'>=</button>
+        </div>
+      </div>
     </div>
   );
 }
