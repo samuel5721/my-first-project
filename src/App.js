e1896330a620be2af7111622200e8a93eb67874a
Kim Samuel <samuel20070731@gmail.com>
2023-07-10 17:49:01 +0900

    초기화

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-import "./calculatorTable.css"; 
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
 function App() {
+  
   return (
     <div className="App">
-      <h1>계산기</h1>
-      <div id="calculator">
-        <div id="window">0</div>
-        <div className='calRow'>
-          <button className='calBtn'>◀</button>
-          <button className='calBtn'>C</button>
-          <button className='calBtn'>%</button>
-          <button className='calBtn'>÷</button>
-        </div>
-        <div className='calRow'>
-          <button className='calBtn'>7</button>
-          <button className='calBtn'>8</button>
-          <button className='calBtn'>9</button>
-          <button className='calBtn'>×</button>
-        </div>
-        <div className='calRow'>
-          <button className='calBtn'>4</button>
-          <button className='calBtn'>5</button>
-          <button className='calBtn'>6</button>
-          <button className='calBtn'>-</button>
-        </div>
-        <div className='calRow'>
-          <button className='calBtn'>1</button>
-          <button className='calBtn'>2</button>
-          <button className='calBtn'>3</button>
-          <button className='calBtn'>+</button>
-        </div>
-        <div className='calRow'>
-          <button className='calBtn'>+/-</button>
-          <button className='calBtn'>0</button>
-          <button className='calBtn'>.</button>
-          <button className='calBtn'>=</button>
-        </div>
-      </div>
+      
     </div>
   );
 }
